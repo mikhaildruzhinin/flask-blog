0533f91f0803076c6c4ef36b3a6ea5040c12805c
Mikhail Druzhinin <druzhininmy@gmail.com>
2022-01-30 17:03:43 +0300

    add dev requirements

--- scripts/install.cmd
+++ scripts/install.cmd
@@ -2,6 +2,7 @@ python -m venv .venv
 call .venv\Scripts\activate
 python -m pip install --upgrade pip
 pip install -r requirements.txt
+pip install -r requirements-dev.txt
 pip install -e .
 set FLASK_APP=app
 flask init-db
