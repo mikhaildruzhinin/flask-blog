7d7d51799b0426906011b009b33d73a2d154f143
Mikhail Druzhinin <druzhininmy@gmail.com>
2022-01-30 16:34:54 +0300

    add distribution file

--- scripts/install.cmd
+++ scripts/install.cmd
@@ -2,5 +2,6 @@ python -m venv .venv
 call .venv\Scripts\activate
 python -m pip install --upgrade pip
 pip install -r requirements.txt
+pip install -e .
 set FLASK_APP=app
 flask init-db
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import (
+    find_packages,
+    setup,
+)
+
+setup(
+    name='blog',
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_file=False,
+    install_requires=[
+        'flask',
+    ],
+)
